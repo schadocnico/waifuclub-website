dd8060b1c4300d5d8e8e03389bdbcc8230cade00
BuildTools <unconfigured@null.spigotmc.org>
2022-03-10 11:08:09 +0100

    v1

---
+++ src/components/App.js
@@ -0,0 +1,82 @@
+import './App.css';
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+import React from 'react';
+import { ReactComponent as Logo } from './waifuClubLogo.svg';
+import Games from './Games';
+
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NoMatch />} />
+          <Route path='/games/' element={<Games />}/>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Header() {
+  return(
+    <div>
+      <nav>
+        <div className="header">
+          <div className="header-contents">
+            <Link to="/">
+              <Logo className="Logo" >
+                <a href='' />
+              </Logo>
+            </Link>
+            <div className="navigation">
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/games">Games</Link>
+                </li>
+              </ul>
+            </div>
+            <div className='antiLogo'>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+// App.js
+function Home() {
+  return (
+    <div>
+      <div className="db1 bg-image">
+
+      </div>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div className='db1 bg-image'>
+      <div className='flex-vertical justify-content-center'>
+        <div className='conteneur'>
+          <h1>404</h1>
+          <h2>Not Found</h2>
+          <p>
+            <Link to="/">Go to the home page</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Games.js
@@ -0,0 +1,46 @@
+import './App.css';
+import './Games.css';
+import React, {useState} from 'react';
+import {countries} from "./data/countries.js";
+import MapChart from "./MapChart";
+
+let paysTrouves = []
+let regions = {'Americas':[], 'Asia':[], 'Africa':[], 'Europe':[], 'Oceania':[], 'Antarctic':[] }
+
+export default function Games() {
+    const [value, setValue] = useState("");
+    const [envoie, setEnvoie] = useState("");
+
+    let handleChange = (event) => {
+        let entree = event.target.value
+        let entree_norm = entree.normalize("NFD").replace(new RegExp("[^(a-zA-Z)]", "g"), '').toUpperCase()
+        for (let i = 0; i < countries.length; i++) {
+            const element = countries[i];
+            const countrie_name = element.name.common;
+            const countrie_name_fr = element.translations.fra.common;
+            if (countrie_name_fr.normalize("NFD").replace(new RegExp("[^(a-zA-Z)]", "g"), '').toUpperCase() === entree_norm) {
+                if(!paysTrouves.includes(countrie_name_fr)){
+                    paysTrouves.push(countrie_name_fr);
+                    setEnvoie(element.cca3);
+                    setValue("");
+                    console.log()
+                } else {
+                    setValue(entree);
+                }
+                return
+            }
+        }
+        setValue(entree);
+    }
+
+    return (
+        <div>
+            <input className='searchPays' type="search" value={value} onChange={(event) => handleChange(event)} />
+            <MapChart inputValue={envoie} />
+        </div>
+    );
+}
+
+function name(params) {
+    
+}
