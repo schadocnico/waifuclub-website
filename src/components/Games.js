c383bb67b01bf7969bcc5f7d3f36223efee63825
BuildTools <unconfigured@null.spigotmc.org>
2022-03-12 15:08:32 +0100

    globe

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,29 +1,31 @@
 import './App.css';
 import './Games.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {countries} from "./data/countries.js";
 import MapChart from "./MapChart";
+import GlobeGame from "./GlobeGame";
 
-let paysTrouves = []
-let regions = {'Americas':[], 'Asia':[], 'Africa':[], 'Europe':[], 'Oceania':[], 'Antarctic':[] }
+//let regions = {'Americas':[], 'Asia':[], 'Africa':[], 'Europe':[], 'Oceania':[], 'Antarctic':[] }
 
 export default function Games() {
     const [value, setValue] = useState("");
-    const [envoie, setEnvoie] = useState("");
+    const [envoie, setEnvoie] = useState([]);
+    const [paysTrouves, setPaysTrouves] = useState([]);
+    const [randomCountrie, setRandomCountrie] = useState(countries[Math.floor(Math.random() * countries.length)]);
+
 
     let handleChange = (event) => {
         let entree = event.target.value
         let entree_norm = entree.normalize("NFD").replace(new RegExp("[^(a-zA-Z)]", "g"), '').toUpperCase()
         for (let i = 0; i < countries.length; i++) {
             const element = countries[i];
-            const countrie_name = element.name.common;
             const countrie_name_fr = element.translations.fra.common;
             if (countrie_name_fr.normalize("NFD").replace(new RegExp("[^(a-zA-Z)]", "g"), '').toUpperCase() === entree_norm) {
                 if(!paysTrouves.includes(countrie_name_fr)){
-                    paysTrouves.push(countrie_name_fr);
-                    setEnvoie(element.cca3);
+                    const dist = Math.abs(Math.round(randomCountrie.latlng[0] - element.latlng[0] + randomCountrie.latlng[1] - element.latlng[1]))
+                    setPaysTrouves([...paysTrouves, countrie_name_fr])
+                    setEnvoie([element.cca3, dist]);
                     setValue("");
-                    console.log()
                 } else {
                     setValue(entree);
                 }
@@ -36,11 +38,29 @@ export default function Games() {
     return (
         <div>
             <input className='searchPays' type="search" value={value} onChange={(event) => handleChange(event)} />
-            <MapChart inputValue={envoie} />
+            <p>{paysTrouves.length}/{countries.length}</p>
+            <GlobeGame inputCountrie={envoie} />
         </div>
     );
 }
 
 function continent() {
-    
+    <table>
+        <thead>
+            <tr>
+                <th>Americas</th>
+                <th>Asia</th>
+                <th>Africa</th>
+                <th>Europe</th>
+                <th>Oceania</th>
+                <th>Antarctic</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>The table body</td>
+                <td>with two columns</td>
+            </tr>
+        </tbody>
+    </table>
 }
---
+++ src/components/GlobeGame.js
@@ -0,0 +1,46 @@
+import './App.css';
+import './Games.css';
+import React, { useState, useEffect, useRef } from 'react';
+import Globe from 'react-globe.gl';
+
+const GlobeGame = (props) => {
+    const globeEl = useRef();
+    const [countries, setCountries] = useState({ features: []});
+    const [countriesInput, setCountriesInput] = useState([]);
+    const [countriesColorInput, setCountriesColorInput] = useState({});
+
+    useEffect(() => {
+      // load data
+      fetch('https://raw.githubusercontent.com/vasturiano/react-globe.gl/master/example/datasets/ne_110m_admin_0_countries.geojson').then(res => res.json())
+        .then(countries=> {
+            //console.log(countries)
+            setCountries(countries);
+        });
+    }, []);
+
+    useEffect(() => {
+        if(props.inputCountrie[0]){
+            setCountriesInput([...countriesInput, props.inputCountrie[0]])
+            setCountriesColorInput({...countriesColorInput, [props.inputCountrie[0]]: props.inputCountrie[1]})
+            console.log(countriesColorInput)
+        }
+    }, [props.inputCountrie]);
+
+    return (
+        <div>
+            <Globe
+                ref={globeEl}
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
+                width = {600}
+                height = {600}
+                backgroundColor = {null}
+                polygonsData={countries.features.filter(d => countriesInput.includes(d.properties.ADM0_A3))}
+                polygonAltitude={0.01}
+                polygonCapColor={(p) => 'rgba(' + Math.abs(countriesColorInput[p.properties.ADM0_A3]*0.95-255) + ', 0, 0, 0.8)'}
+                polygonSideColor={() => 'rgba(0, 100, 0, 0.15)'}
+            />
+        </div>
+    );
+};
+
+export default GlobeGame;
